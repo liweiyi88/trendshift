3eee017d4d41b121a8c111115ef756b6f64195bb
liweiyi88 <weiyi.li713@gmail.com>
2023-08-14 15:37:18 +1000

    use biz chart

--- components/Widget.tsx
+++ components/Widget.tsx
@@ -1,17 +1,20 @@
 import classNames from 'classnames'
 
 interface Props {
+  children?: React.ReactNode
   className?: string
 }
 
-const Widget = ({ className }: Props) => {
+const Widget = ({ className, children }: Props) => {
   return (
     <div
       className={classNames(
         'bg-white rounded-lg border border-gray-200',
         className,
       )}
-    ></div>
+    >
+      {children}
+    </div>
   )
 }
 
